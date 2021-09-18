8b4be104b3238e53807b987407fef3377ca49b2c
Isaac Bruno Quiroga <isbrqu@gmail.com>
2021-09-18 17:41:30 -0300

    pasa el segundo login

--- bpn.py
+++ bpn.py
@@ -17,7 +17,9 @@ class Bpn(object):
 
     def __login(self, username, password, is_inclu, pin):
         soup = self.__first_login(username, is_inclu, pin)
-        return soup
+        state = soup.select_one('#LoginForm input[name="_STATE_"]')['value']
+        soup = self.__second_login(username, password, state)
+        self.soup = soup
 
     def __first_login(self, username, is_inclu, pin):
         params = {
@@ -28,5 +30,24 @@ class Bpn(object):
         url = bpn_url.first_login
         header = bpn_header.login
         response = self.session.post(url, params=params, headers=header)
-        self.soup = Soup(response.text, PARSER)
+        soup = Soup(response.text, PARSER)
+        return soup
+
+    def __second_login(self, username, password, state):
+        params = {
+            'username': username,
+            'password': password,
+            'jsonRequest': True,
+            'sfaInfo': '',
+            'pcCompartida': True,
+            'inclu': False,
+            'recordarUsuario': False,
+            '_STATE_': state,
+        }
+        url = bpn_url.second_login
+        header = bpn_header.dologin
+        response = self.session.post(url, params=params, headers=header)
+        soup = Soup(response.text, PARSER)
+        return soup
+
 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ username = config.username
 password = config.password
 
 bpn = Bpn(username, password)
-with open('login.html', 'w', encoding='utf-8') as html:
+with open('dologin.html', 'w', encoding='utf-8') as html:
     text = bpn.soup.prettify()
     html.write(text)
 
