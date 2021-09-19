fbacb8d0ae0b66503b6156a81ab4d9649315792b
Isaac Bruno Quiroga <isbrqu@gmail.com>
2021-09-19 17:39:06 -0300

    obtiene el state de consultaCargaValorTP

--- bpn.py
+++ bpn.py
@@ -114,6 +114,12 @@ class Bpn(object):
         state = tag['realhref'].split('=')[1]
         print(state)
 
+    def phone_recharge(self):
+        selector = '#_menu_consultaCargaValorTP'
+        tag = self.soup_home.select_one(selector)
+        state = tag['realhref'].split('=')[1]
+        print(state)
+
     def balance(self):
         params = {
             '_STATE_': self.states['posicionConsolidada'],
