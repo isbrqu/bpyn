ae2450a5e8ef6f1c9fe94ae66219b324d14ffe15
Isaac Bruno Quiroga <isbrqu@gmail.com>
2022-01-02 14:16:55 -0300

    modifica el main con algunos casos de usos

--- src/main.py
+++ src/main.py
@@ -1,14 +1,34 @@
 from bpn import Bpn
 import config
 from pprint import pprint
+import yaml
 
 username = config.username
 password = config.password
 
-bpn = Bpn(username, password)
-
 try:
-    pprint(bpn.accounts_transferences)
+    bpn = Bpn(username, password)
+    # value = bpn.b
+    # print(value)
+    # value = values
+    for value in bpn.third_party_transfer_accounts:
+        for data in value['response']['data']:
+            pertenece = data['perteneceA']
+            cbu = data['numeroCbu']
+            # print(yaml.dump(value))
+            print(cbu, pertenece)
+    # print(yaml.dump(value))
+    # value = bpn.accounts
+    # print(yaml.dump(value))
+    # value = bpn.accounts_for_pc
+    # print(yaml.dump(value))
+    # value = bpn.entities
+    # print(yaml.dump(value['response']['data'][0]['adheridos']))
+    # value = bpn.unknown_transfer_accounts
+    # print(yaml.dump(value))
+    # print(value)
+    # print('='*20)
 finally:
-    print(bpn.logout())
+    page = bpn.logout()
+    print(page.url)
 
